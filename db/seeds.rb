b7b36f97eb6647868775d6f7e807cf2b68b3ea95
lucas <lucas.alek@gmail.com>
2018-02-26 13:50:37 -0600

    this wont work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+car_file =  JSON.parse(open("#{Rails.root}/db/assets/json_data.json").read)
+
+car_file.each do |car|
+    car.create(make: car['make'],
+               model: car['model'])
+
+               
+    car_year = car['year']
+    year_exist = Year.where("year == car_year")
+    if(!car_year){
+        year.create(year: car['year'])
+    }
+    
+end 
